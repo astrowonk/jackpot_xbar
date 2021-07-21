8aba8ed539f9e4df12510ebdb59128f1b5f7596e
Marcos Huerta <sirmarcos@gmail.com>
2021-07-20 23:26:16 -0400

    fix font size which is somehow ignored in swiftbar

--- jackpot.6h.py
+++ jackpot.6h.py
@@ -144,10 +144,10 @@ class Jackpot():
         print(self.icon_row)
         print('---')
         print(
-            f"MM: {mm_str: >7} - {self.get_next_drawing_date(['tue','fri'])} | font='Menlo' | size=18 | color={self.mega_color} href=https://www.megamillions.com"
+            f"MM: {mm_str: >7} - {self.get_next_drawing_date(['tue','fri'])} | font='Menlo' | size=13 | color={self.mega_color} href=https://www.megamillions.com"
         )
         print(
-            f"PB: {pb_str: >7} - {self.get_next_drawing_date(['wed','sat'])} | font='Menlo' | size=18 | color={self.pb_color} href=https://www.powerball.com"
+            f"PB: {pb_str: >7} - {self.get_next_drawing_date(['wed','sat'])} | font='Menlo' | size=13 | color={self.pb_color} href=https://www.powerball.com"
         )
 
     @staticmethod
